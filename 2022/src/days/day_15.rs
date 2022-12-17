892eee081c0675ccfd2abdc0181da058d0df6eff
flimblewitz <9010741+flimblewitz@users.noreply.github.com>
2022-12-16 21:57:49 -0500

    2022 day 15 part 2

--- 2022/src/days/day_15.rs
+++ 2022/src/days/day_15.rs
@@ -2,14 +2,20 @@ use regex::Regex;
 
 #[derive(Debug)]
 struct Sensor {
-    x: i32,
-    y: i32,
-    range: i32,
+    x: i64,
+    y: i64,
+    range: i64,
 }
+impl Sensor {
+    fn can_sense(&self, (x, y): (i64, i64)) -> bool {
+        self.range >= (self.x.abs_diff(x) + self.y.abs_diff(y)) as i64
+    }
+}
+
 #[derive(Clone, Debug, PartialEq)]
 struct Beacon {
-    x: i32,
-    y: i32,
+    x: i64,
+    y: i64,
 }
 
 fn parse_input(input: &str) -> (Vec<Sensor>, Vec<Beacon>) {
@@ -21,13 +27,13 @@ fn parse_input(input: &str) -> (Vec<Sensor>, Vec<Beacon>) {
         re.captures_iter(input)
             .fold((vec![], vec![]), |(mut sensors, mut beacons), captures| {
                 // println!("regex recognizes a line! {:?}", captures);
-                let sensor_x: i32 = captures[1].parse().unwrap();
-                let sensor_y: i32 = captures[2].parse().unwrap();
-                let beacon_x: i32 = captures[3].parse().unwrap();
-                let beacon_y: i32 = captures[4].parse().unwrap();
+                let sensor_x: i64 = captures[1].parse().unwrap();
+                let sensor_y: i64 = captures[2].parse().unwrap();
+                let beacon_x: i64 = captures[3].parse().unwrap();
+                let beacon_y: i64 = captures[4].parse().unwrap();
 
                 let manhattan_distance =
-                    (sensor_x.abs_diff(beacon_x) + sensor_y.abs_diff(beacon_y)) as i32;
+                    (sensor_x.abs_diff(beacon_x) + sensor_y.abs_diff(beacon_y)) as i64;
 
                 sensors.push(Sensor {
                     x: sensor_x,
@@ -49,17 +55,17 @@ fn parse_input(input: &str) -> (Vec<Sensor>, Vec<Beacon>) {
     sensors_and_beacons
 }
 
-pub fn part_one(input: &str) -> u32 {
+pub fn part_one(input: &str) -> u64 {
     let (sensors, beacons) = parse_input(input);
     // println!("sensors: {:?}", sensors);
     // println!("beacons: {:?}", beacons);
 
     // so now that I have a list of sensors, I can take the given row of interest, 2000000, and map each sensor to the range of x indexes in that row that are within range of that sensor
-    // let y_of_interest: i32 = 10;
-    let y_of_interest: i32 = 2000000;
+    // let y_of_interest: i64 = 10;
+    let y_of_interest: i64 = 2000000;
 
     let ranges_of_x_indexes_visible_to_sensors = sensors.iter().filter_map(|s| {
-        let abs_y_distance = y_of_interest.abs_diff(s.y) as i32;
+        let abs_y_distance = y_of_interest.abs_diff(s.y) as i64;
         let x_range_on_row_of_interest = s.range - abs_y_distance;
         match abs_y_distance <= s.range {
             true => {
@@ -81,7 +87,7 @@ pub fn part_one(input: &str) -> u32 {
     });
 
     // let's combine the ones that are overlapping
-    let disparate_ranges_of_x_indexes_visible_to_sensors: Vec<(i32, i32)> =
+    let disparate_ranges_of_x_indexes_visible_to_sensors: Vec<(i64, i64)> =
         ranges_of_x_indexes_visible_to_sensors.fold(
             vec![],
             |mut ranges, range_of_x_indexes_visible_to_a_particular_sensor| {
@@ -121,7 +127,7 @@ pub fn part_one(input: &str) -> u32 {
     // );
 
     let number_of_beacons_at_y_of_interest =
-        beacons.iter().filter(|b| b.y == y_of_interest).count() as u32;
+        beacons.iter().filter(|b| b.y == y_of_interest).count() as u64;
     // println!(
     //     "number_of_beacons_at_y_of_interest: {}",
     //     number_of_beacons_at_y_of_interest
@@ -130,29 +136,61 @@ pub fn part_one(input: &str) -> u32 {
     number_of_x_indexes_visible_to_sensors - number_of_beacons_at_y_of_interest
 }
 
-pub fn part_two(input: &str) -> i32 {
+// const MAX_X_AND_Y: i64 = 20;
+const MAX_X_AND_Y: i64 = 4000000;
+
+fn get_tuning_value_of_coord_if_hidden(coord: (i64, i64), sensors: &[Sensor]) -> Option<i64> {
+    if coord.0 >= 0 && coord.0 <= MAX_X_AND_Y && coord.1 >= 0 && coord.1 <= MAX_X_AND_Y {
+        // println!("considering coord {:?}", coord);
+        if !sensors.iter().any(|s| {
+            let sensed = s.can_sense(coord);
+            // println!("sensor {:?} can sense it: {}", s, sensed);
+            sensed
+        }) {
+            return Some(get_tuning_value(coord));
+        }
+    }
+    None
+}
+
+fn get_tuning_value((x, y): (i64, i64)) -> i64 {
+    x * 4000000 + y
+}
+
+pub fn part_two(input: &str) -> i64 {
     let (sensors, _) = parse_input(input);
 
-    // let max_x_and_y = 20;
-    let max_x_and_y = 4000000;
+    // if there's just one coord outside of every sensor's range, that means we don't have to actually scan over 16 million coords: we only have to scan over all the coords immediately adjacent to the ones at the edge of each sensor's range
+    // for each sensor, for each in-bounds coord immediately adjacent to its range, check if it's in range of any other sensor
+    // if it's not in range of any other sensor, we've found our hidden coord and can return its tuning value
+    // else, if we've just looked at every coord adjacent to the given sensor's range, remove the sensor from consideration and move onto the next sensor
+    for sensor in sensors.iter() {
+        // println!("considering sensor {:?}", sensor);
+        for y in 0.max(sensor.y - sensor.range)..=MAX_X_AND_Y.min(sensor.y + sensor.range) {
+            let y_dist_from_sensor = sensor.y.abs_diff(y) as i64;
+            // println!("considering y_dist_from_sensor: {}", y_dist_from_sensor);
 
-    panic!("part two isn't yet working for real input");
+            // check both x coords at this y
+            let x_dist_from_sensor = sensor.range - y_dist_from_sensor;
+            // println!("considering x_dist_from_sensor: {}", x_dist_from_sensor);
 
-    for x in 0..=max_x_and_y {
-        for y in 0..=max_x_and_y {
-            if !sensors.iter().any(|s| {
-                let is_in_range = ((s.x.abs_diff(x) + s.y.abs_diff(y)) as i32) <= s.range;
-                println!(
-                    "({}, {}) in range of sensor at ({}, {}) with range {}: {}",
-                    x, y, s.x, s.y, s.range, is_in_range
-                );
-                is_in_range
-            }) {
-                // println!(
-                //     "coordinate ({}, {}) is outside the range of any sensor",
-                //     x, y
-                // );
-                return x * 4000000 + y;
+            let left_x = sensor.x - x_dist_from_sensor;
+            // println!("considering in-range left coord ({}, {})", left_x, y);
+            let coord_left_of_left_x = (left_x - 1, y);
+
+            let right_x = sensor.x + x_dist_from_sensor;
+            // println!("considering in-range right coord ({}, {})", right_x, y);
+            let coord_right_of_right_x = (right_x + 1, y);
+
+            let coords = vec![coord_left_of_left_x, coord_right_of_right_x];
+            // println!("considering coords {:?}", coords);
+
+            if let Some(tuning_value) = coords
+                .iter()
+                .filter_map(|coord| get_tuning_value_of_coord_if_hidden(*coord, &sensors))
+                .next()
+            {
+                return tuning_value;
             }
         }
     }
