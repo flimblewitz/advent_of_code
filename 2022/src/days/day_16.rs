d3c0e734861f98db7986e538a14afdaf94c425ae
flimblewitz <9010741+flimblewitz@users.noreply.github.com>
2022-12-18 00:34:17 -0500

    2022 day 16 part 2, but it times out for the real input

--- 2022/src/days/day_16.rs
+++ 2022/src/days/day_16.rs
@@ -2,6 +2,7 @@ use regex::Regex;
 use std::collections::HashMap;
 
 const MINUTES_UNTIL_ERUPTION: usize = 30;
+const MINUTES_UNTIL_ERUPTION_WITH_ELEPHANT: usize = 26;
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 struct Valve {
@@ -185,7 +186,6 @@ fn part_one_recursively_get_max_pressure_released(
 }
 
 pub fn part_one(input: &str) -> usize {
-    // return 0;
     println!("be warned that part 1 takes several minutes for the real input");
     let (names_to_indexes, mut valves) = parse_input(input);
     // println!("valves: {:?}", valves);
@@ -199,6 +199,326 @@ pub fn part_one(input: &str) -> usize {
         &mut sub_solutions,
     )
 }
+
+fn part_two_recursively_get_max_pressure_released(
+    minutes_left: usize,
+    valves: &mut Vec<Valve>,
+    starting_valve_indexes: [usize; 2],
+    sub_solutions: &mut HashMap<(usize, Vec<Valve>, [usize; 2]), (usize, String)>,
+) -> (usize, String) {
+    if let Some(sub_solution) =
+        sub_solutions.get(&(minutes_left, valves.clone(), starting_valve_indexes))
+    {
+        // println!(
+        //     "{}memoized: {}",
+        //     (minutes_left..MINUTES_UNTIL_ERUPTION_WITH_ELEPHANT)
+        //         .map(|_| ' ')
+        //         .collect::<String>(),
+        //     sub_solution
+        // );
+
+        return sub_solution.clone();
+    }
+
+    let valve_1 = valves[starting_valve_indexes[0]].clone();
+    let valve_2 = valves[starting_valve_indexes[1]].clone();
+
+    let current_minute = MINUTES_UNTIL_ERUPTION_WITH_ELEPHANT - minutes_left + 1;
+    let open_valves: Vec<&Valve> = valves.iter().filter(|v| v.open).collect();
+    let open_valve_names: Vec<&String> = open_valves.iter().map(|v| &v.name).collect();
+    let open_valve_flow_rate_sum: usize = open_valves.iter().map(|v| v.flow_rate).sum();
+    let report = format!(
+        r"== Minute {} ==
+Valves {:?} are open, releasing {} pressure
+First entity is at valve {}
+Second entity is at valve {}
+    ",
+        current_minute, open_valve_names, open_valve_flow_rate_sum, valve_1.name, valve_2.name
+    );
+
+    if valves.iter().all(|v| v.open || v.flow_rate == 0) || minutes_left <= 2 {
+        // println!(
+        //     "{}no more pressure can be released",
+        //     (minutes_left..MINUTES_UNTIL_ERUPTION_WITH_ELEPHANT)
+        //         .map(|_| ' ')
+        //         .collect::<String>(),
+        // );
+        return (0, report);
+    }
+
+    // println!(
+    //     "{}with {} minutes_left, starting at valves {} and {}",
+    //     (minutes_left..MINUTES_UNTIL_ERUPTION_WITH_ELEPHANT)
+    //         .map(|_| ' ')
+    //         .collect::<String>(),
+    //     minutes_left,
+    //     valve_1.name,
+    //     valve_2.name,
+    // );
+
+    // there are four possibilities:
+    // 1. both distinct valves are opened
+    // 2. only valve_1 is opened
+    // 2. only valve_2 is opened
+    // 3. zero valves are opened
+
+    let all_adjacent_valve_indexes_combinations: Vec<[usize; 2]> = valve_1
+        .adjacent_valves
+        .iter()
+        .map(|(_, adjacent_valve_1_index)| {
+            valve_2
+                .adjacent_valves
+                .iter()
+                .map(|(_, adjacent_valve_2_index)| {
+                    let mut next_valve_indexes = [*adjacent_valve_1_index, *adjacent_valve_2_index];
+                    // it's important to sort each of these "tuples" because that way we can actually make optimal use of the memoization
+                    // there's no need to care who's who between santa and the elephant; all that matters is that one is at valve X and one is at valve Y
+                    // sorting allows us to care about the combination of valves that they're at instead of the permutation
+                    next_valve_indexes.sort();
+                    next_valve_indexes
+                })
+                .collect()
+        })
+        .collect::<Vec<Vec<[usize; 2]>>>()
+        .into_iter()
+        .flatten()
+        .collect();
+
+    // println!(
+    //     "all_adjacent_valve_indexes_combinations: {:?}",
+    //     all_adjacent_valve_indexes_combinations
+    // );
+
+    let solution_if_opening_two_valves = if !valve_1.open
+        && valve_1.flow_rate > 0
+        && !valve_2.open
+        && valve_2.flow_rate > 0
+        && valve_1.name != valve_2.name
+        && minutes_left >= 2
+    {
+        let pressure_released_by_opening_valve_1 = valve_1.flow_rate * (minutes_left - 1);
+        let pressure_released_by_opening_valve_2 = valve_2.flow_rate * (minutes_left - 1);
+
+        valves[starting_valve_indexes[0]].open = true;
+        valves[starting_valve_indexes[1]].open = true;
+
+        let (max_pressure_released_afterward, report_afterward) =
+            all_adjacent_valve_indexes_combinations
+                .iter()
+                .map(|adjacent_valve_indexes_combination| {
+                    part_two_recursively_get_max_pressure_released(
+                        minutes_left - 2,
+                        valves,
+                        *adjacent_valve_indexes_combination,
+                        sub_solutions,
+                    )
+                })
+                .max_by_key(|s| s.0)
+                .unwrap();
+
+        let max_pressure_released_by_opening_valves = pressure_released_by_opening_valve_1
+            + pressure_released_by_opening_valve_2
+            + max_pressure_released_afterward;
+
+        valves[starting_valve_indexes[0]].open = false;
+        valves[starting_valve_indexes[1]].open = false;
+
+        (
+            max_pressure_released_by_opening_valves,
+            format!("{}\n{}", report, report_afterward),
+        )
+    } else {
+        (0, "".into())
+    };
+
+    // println!(
+    //     "{}max_pressure_released_if_opening_two_valves: {}",
+    //     (minutes_left..MINUTES_UNTIL_ERUPTION_WITH_ELEPHANT)
+    //         .map(|_| ' ')
+    //         .collect::<String>(),
+    //     max_pressure_released_if_opening_two_valves
+    // );
+
+    // todo: is my minutes_left math right?
+    let solution_if_opening_valve_1 = if !valve_1.open && valve_1.flow_rate > 0 && minutes_left >= 2
+    {
+        let pressure_released_by_opening_valve_1 = valve_1.flow_rate * (minutes_left - 1);
+
+        valves[starting_valve_indexes[0]].open = true;
+
+        let valve_indexes_combinations_for_valve_2_adjacent_indexes: Vec<[usize; 2]> = valve_2
+            .adjacent_valves
+            .iter()
+            .map(|(_, adjacent_valve_2_index)| {
+                let mut next_valve_indexes = [starting_valve_indexes[0], *adjacent_valve_2_index];
+                // it's important to sort each of these "tuples" because that way we can actually make optimal use of the memoization
+                // there's no need to care who's who between santa and the elephant; all that matters is that one is at valve X and one is at valve Y
+                // sorting allows us to care about the combination of valves that they're at instead of the permutation
+                next_valve_indexes.sort();
+                next_valve_indexes
+            })
+            .collect();
+
+        let (max_pressure_released_afterward, report_afterward) =
+            valve_indexes_combinations_for_valve_2_adjacent_indexes
+                .iter()
+                .map(|valve_indexes_combination| {
+                    part_two_recursively_get_max_pressure_released(
+                        minutes_left - 1,
+                        valves,
+                        *valve_indexes_combination,
+                        sub_solutions,
+                    )
+                })
+                .max_by_key(|s| s.0)
+                .unwrap();
+
+        let max_pressure_released_after_opening_valve_1 =
+            pressure_released_by_opening_valve_1 + max_pressure_released_afterward;
+
+        valves[starting_valve_indexes[0]].open = false;
+
+        (
+            max_pressure_released_after_opening_valve_1,
+            format!("{}\n{}", report, report_afterward),
+        )
+    } else {
+        (0, "".into())
+    };
+
+    // println!(
+    //     "{}max_pressure_released_if_opening_valve_1: {}",
+    //     (minutes_left..MINUTES_UNTIL_ERUPTION_WITH_ELEPHANT)
+    //         .map(|_| ' ')
+    //         .collect::<String>(),
+    //     max_pressure_released_if_opening_valve_1
+    // );
+
+    // todo: is my minutes_left math right?
+    let solution_if_opening_valve_2 = if !valve_2.open && valve_2.flow_rate > 0 && minutes_left >= 2
+    {
+        let pressure_released_by_opening_valve_2 = valve_2.flow_rate * (minutes_left - 1);
+
+        valves[starting_valve_indexes[1]].open = true;
+
+        let valve_indexes_combinations_for_valve_1_adjacent_indexes: Vec<[usize; 2]> = valve_1
+            .adjacent_valves
+            .iter()
+            .map(|(_, adjacent_valve_1_index)| {
+                let mut next_valve_indexes = [*adjacent_valve_1_index, starting_valve_indexes[1]];
+                // it's important to sort each of these "tuples" because that way we can actually make optimal use of the memoization
+                // there's no need to care who's who between santa and the elephant; all that matters is that one is at valve X and one is at valve Y
+                // sorting allows us to care about the combination of valves that they're at instead of the permutation
+                next_valve_indexes.sort();
+                next_valve_indexes
+            })
+            .collect();
+
+        let (max_pressure_released_afterward, report_afterward) =
+            valve_indexes_combinations_for_valve_1_adjacent_indexes
+                .iter()
+                .map(|valve_indexes_combination| {
+                    part_two_recursively_get_max_pressure_released(
+                        minutes_left - 1,
+                        valves,
+                        *valve_indexes_combination,
+                        sub_solutions,
+                    )
+                })
+                .max_by_key(|s| s.0)
+                .unwrap();
+
+        let max_pressure_released_after_opening_valve_2 =
+            pressure_released_by_opening_valve_2 + max_pressure_released_afterward;
+
+        valves[starting_valve_indexes[1]].open = false;
+
+        (
+            max_pressure_released_after_opening_valve_2,
+            format!("{}\n{}", report, report_afterward),
+        )
+    } else {
+        (0, "".into())
+    };
+
+    // println!(
+    //     "{}max_pressure_released_if_opening_valve_2: {}",
+    //     (minutes_left..MINUTES_UNTIL_ERUPTION_WITH_ELEPHANT)
+    //         .map(|_| ' ')
+    //         .collect::<String>(),
+    //     max_pressure_released_if_opening_valve_2
+    // );
+
+    let mut solution_after_opening_zero_valves = all_adjacent_valve_indexes_combinations
+        .iter()
+        .map(|adjacent_valve_indexes_combination| {
+            part_two_recursively_get_max_pressure_released(
+                minutes_left - 1,
+                valves,
+                *adjacent_valve_indexes_combination,
+                sub_solutions,
+            )
+        })
+        .max_by_key(|s| s.0)
+        .unwrap();
+    solution_after_opening_zero_valves.1 =
+        format!("{}\n{}", report, solution_after_opening_zero_valves.1);
+
+    // println!(
+    //     "{}max_pressure_released_after_opening_zero_valves: {}",
+    //     (minutes_left..MINUTES_UNTIL_ERUPTION_WITH_ELEPHANT)
+    //         .map(|_| ' ')
+    //         .collect::<String>(),
+    //     max_pressure_released_after_opening_zero_valves
+    // );
+
+    // let solution = solution_if_opening_two_valves
+    //     .max(solution_if_opening_valve_1)
+    //     .max(solution_if_opening_valve_2)
+    //     .max(solution_after_opening_zero_valves);
+    let potential_solutions = vec![
+        solution_if_opening_two_valves,
+        solution_if_opening_valve_1,
+        solution_if_opening_valve_2,
+        solution_after_opening_zero_valves,
+    ];
+    let solution = potential_solutions
+        .iter()
+        .max_by_key(|(pressure_released, _)| pressure_released)
+        .unwrap();
+
+    // println!(
+    //     "{}... max_pressure_released: {}",
+    //     (minutes_left..MINUTES_UNTIL_ERUPTION_WITH_ELEPHANT)
+    //         .map(|_| ' ')
+    //         .collect::<String>(),
+    //     max_pressure_released
+    // );
+
+    sub_solutions.insert(
+        (minutes_left, valves.clone(), starting_valve_indexes),
+        solution.clone(),
+    );
+
+    solution.clone()
+}
+
 pub fn part_two(input: &str) -> usize {
-    todo!()
+    let (names_to_indexes, mut valves) = parse_input(input);
+
+    let mut sub_solutions: HashMap<(usize, Vec<Valve>, [usize; 2]), (usize, String)> =
+        HashMap::new();
+
+    let starting_valve_index = *names_to_indexes.get("AA").unwrap();
+
+    let (pressure_released, _report) = part_two_recursively_get_max_pressure_released(
+        MINUTES_UNTIL_ERUPTION_WITH_ELEPHANT,
+        &mut valves,
+        [starting_valve_index, starting_valve_index],
+        &mut sub_solutions,
+    );
+
+    // println!("{}", _report);
+
+    pressure_released
 }
