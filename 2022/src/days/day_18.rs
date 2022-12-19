7ade2071f636edcdfc7b9e771e26014b02bcf12e
flimblewitz <9010741+flimblewitz@users.noreply.github.com>
2022-12-18 19:09:28 -0500

    2022 day 18 part 2 note (unsolved)

--- 2022/src/days/day_18.rs
+++ 2022/src/days/day_18.rs
@@ -12,7 +12,7 @@ fn parse_input(input: &str) -> HashSet<(i16, i16, i16)> {
     })
 }
 
-fn does_not_exist_to_u16(cube: (i16, i16, i16), cubes: &HashSet<(i16, i16, i16)>) -> usize {
+fn does_not_exist_to_usize(cube: (i16, i16, i16), cubes: &HashSet<(i16, i16, i16)>) -> usize {
     if cubes.contains(&cube) {
         0
     } else {
@@ -22,20 +22,71 @@ fn does_not_exist_to_u16(cube: (i16, i16, i16), cubes: &HashSet<(i16, i16, i16)>
 
 pub fn part_one(input: &str) -> usize {
     let cubes = parse_input(input);
-    println!("{:?}", cubes);
+    // println!("{:?}", cubes);
     cubes
         .iter()
         .map(|cube| {
-            does_not_exist_to_u16((cube.0 - 1, cube.1, cube.2), &cubes)
-                + does_not_exist_to_u16((cube.0 + 1, cube.1, cube.2), &cubes)
-                + does_not_exist_to_u16((cube.0, cube.1 - 1, cube.2), &cubes)
-                + does_not_exist_to_u16((cube.0, cube.1 + 1, cube.2), &cubes)
-                + does_not_exist_to_u16((cube.0, cube.1, cube.2 - 1), &cubes)
-                + does_not_exist_to_u16((cube.0, cube.1, cube.2 + 1), &cubes)
+            get_adjacent_cubes(*cube)
+                .iter()
+                .map(|ac| does_not_exist_to_usize(*ac, &cubes))
+                .sum::<usize>()
         })
         .sum()
 }
 
-pub fn part_two(input: &str) -> usize {
-    todo!()
+fn get_adjacent_cubes(cube: (i16, i16, i16)) -> [(i16, i16, i16); 6] {
+    [
+        (cube.0 - 1, cube.1, cube.2),
+        (cube.0 + 1, cube.1, cube.2),
+        (cube.0, cube.1 - 1, cube.2),
+        (cube.0, cube.1 + 1, cube.2),
+        (cube.0, cube.1, cube.2 - 1),
+        (cube.0, cube.1, cube.2 + 1),
+    ]
+}
+
+fn _does_not_exist_and_is_not_surrounded_to_usize(
+    cube: (i16, i16, i16),
+    cubes: &HashSet<(i16, i16, i16)>,
+) -> usize {
+    let adjacent_cubes = get_adjacent_cubes(cube);
+
+    let is_surrounded_by_existing_cubes = adjacent_cubes
+        .iter()
+        .all(|adjacent_cube| cubes.contains(&adjacent_cube));
+
+    // if is_surrounded_by_existing_cubes {
+    //     println!(
+    //         "{} surrounded by {}",
+    //         format!("({},{},{})", cube.0, cube.1, cube.2),
+    //         adjacent_cubes
+    //             .map(|ac| format!("({},{},{})", ac.0, ac.1, ac.2))
+    //             .join(", ")
+    //     );
+    // }
+
+    if cubes.contains(&cube) || is_surrounded_by_existing_cubes {
+        0
+    } else {
+        1
+    }
+}
+
+// wrong: 4338 (high)
+pub fn part_two(_input: &str) -> usize {
+    // let cubes = parse_input(input);
+    // println!("{:?}", cubes);
+
+    // we are counting the number of nonexistent or non surrounded cubes
+    // cubes
+    //     .iter()
+    //     .map(|cube| {
+    //         get_adjacent_cubes(*cube)
+    //             .iter()
+    //             .map(|ac| _does_not_exist_and_is_not_surrounded_to_usize(*ac, &cubes))
+    //             .sum::<usize>()
+    //     })
+    //     .sum()
+
+    todo!("I need to consider pockets that are bigger than just the size of one cube")
 }
